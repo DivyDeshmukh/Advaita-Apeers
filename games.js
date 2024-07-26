cb69c4f5d0747483c2189e08edc5645504785f5b
Divy Deshmukh <127101632+DivyDeshmukh@users.noreply.github.com>
2024-07-26 19:40:21 +0530

    Update games.js

--- games.js
+++ games.js
@@ -34,7 +34,7 @@ document.querySelector("#mobile").addEventListener("click", function() {
 
 
 document.querySelector("#mobile #about").addEventListener("click", function() {
-    alert("Hey there! Divy this side, please check out this amazing landing page designed by me. It showcase the ancient culture and wisdom of our Bharat. There are several features i want you to take into consideration in menu like the knowledge section, architecture section, foods, and you can also play games in the games section. Please read the article on knowledge section. THANKYOU FOR VISITING...");
+    alert("Hey there! Divy this side, please check out this amazing project developed by me. It showcase the ancient culture and wisdom of our Bharat. There are several features i want you to take into consideration in menu like the knowledge section, architecture section, foods, and you can also play games in the games section. Please read the article on knowledge section. THANKYOU FOR VISITING...");
   });
   
   document.querySelector("#mobile #contact").addEventListener("click", function() {
@@ -42,7 +42,7 @@ document.querySelector("#mobile #about").addEventListener("click", function() {
   });
   
   document.getElementsByTagName("h4")[1].addEventListener("click", function() {
-    alert("Hey there! Divy this side, please check out this amazing landing page designed by me. It showcase the ancient culture and wisdom of our Bharat.  Please read the article on knowledge section. There are several features i want you to take into consideration in menu like the knowledge section, architecture section, foods, and you can also play games in the games section. THANKYOU FOR VISITING...");
+    alert("Hey there! Divy this side, please check out this amazing project developed by me. It showcase the ancient culture and wisdom of our Bharat.  Please read the article on knowledge section. There are several features i want you to take into consideration in menu like the knowledge section, architecture section, foods, and you can also play games in the games section. THANKYOU FOR VISITING...");
   });
   
   document.getElementsByTagName("h4")[2].addEventListener("click", function() {
